6379c23b51759981877ce0009404e458be588795
Akaki Jvarsheishvili <kakijvarsheishvili3@gmail.com>
2026-05-11 20:23:16 -0400

    fix css mime type

--- server/index.js
+++ server/index.js
@@ -17,8 +17,13 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client')));
-
+app.use(express.static(path.join(__dirname, '../client'), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/chat', chatRoutes);
